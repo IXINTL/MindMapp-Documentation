c193a5624c82a4ec22489e15cdcfc266c050e01f
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-22 15:51:55 +0530

    [chore] updated basic config for deployment.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'MindMapp',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -16,12 +16,13 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'MindMapp-Documentation',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'IXINTL', // Usually your GitHub org/user name.
+  trailingSlash: false,
+  projectName: 'MindMapp-Documentation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
