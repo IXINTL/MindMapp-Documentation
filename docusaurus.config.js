82adad403b2e838b198fd06e77b6920b25a5b56f
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-22 16:34:52 +0530

    set the sidebar collapsible -> false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,6 +43,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
+          sidebarCollapsible: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -68,7 +69,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'DOCUMENTATION | MINDMAPP',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
